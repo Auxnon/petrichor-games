9039788ab81c2b2a86fdbf505fbb6244b49fdf58
Makeavoy <auxnon@gmail.com>
2023-09-30 00:52:45 -0400

    feat: added maze board game poc

--- ignore.lua
+++ ignore.lua
@@ -1,5 +1,6 @@
--- Codex 2.0.0 "Avocado"
+-- Codex 3.0.0 "Artichoke"
 ---@diagnostic disable: duplicate-doc-field, missing-return
+---@meta
 
 ---@class mouse
 ---@field x number
@@ -9,6 +10,7 @@
 ---@field m1 boolean mouse 1
 ---@field m2 boolean mouse 2
 ---@field m3 boolean mouse 3
+-- @field scroll number scroll delta
 ---@field vx number unprojection x
 ---@field vy number unprojection y
 ---@field vz number unprojection z
@@ -54,102 +56,304 @@
 --- @field vz number velocity z
 --- @field flipped number texture flip x axis
 --- @field scale number uniform scale factor 1 is 100%
+--- @field offset number[]? x, y, z
 --- @field id integer assigned by engine, killable
 --- @field tex string texture asset
 --- @field asset string model or blocked texture asset
---- @field anim fun(animation:string,force?:boolean) change animation, force marks change even if already playing
---- @field kill fun() destroy entity
+--- @field anim fun(self:entity,animation:string,force?:boolean) change animation, force marks change even if already playing
+--- @field kill fun(self:entity) destroy entity
 
 --- @alias gunit number | integer | string
 --- @alias rgb number[] | integer[] | string
 
 --- @class image
---- @field line fun(x:gunit, y:gunit, x2:gunit, y2:gunit, rgb?:rgb) draw line on image
---- @field rect fun(x:gunit, y:gunit, w:gunit, h:gunit, rgb?:rgb) draw rectangle on image
---- @field rrect fun(x:gunit, y:gunit, w:gunit, h:gunit,ro:gunit, rgb?:rgb) draw rounded rectangle on image
---- @field text fun(txt:string, x?:gunit, y?:gunit, rgb?:rgb) draw text on image
---- @field img fun(im:image, x?:gunit, y?:gunit) draw another image on image
---- @field clr fun() clear image
---- @field fill fun(rgb?:rgb) fill image with color
---- @field raw fun():integer[] image return raw pixel data
---- @field copy fun():image clones to new image
+--- @field line fun(self:image, x:gunit, y:gunit, x2:gunit, y2:gunit, rgb?:rgb) draw line on image
+--- @field rect fun(self:image, x:gunit, y:gunit, w:gunit, h:gunit, rgb?:rgb) draw rectangle on image
+--- @field rrect fun(self:image ,x:gunit, y:gunit, w:gunit, h:gunit,ro:gunit, rgb?:rgb) draw rounded rectangle on image
+--- @field text fun(self:image, txt:string, x?:gunit, y?:gunit, rgb?:rgb) draw text on image
+--- @field img fun(self:image, im:image, x?:gunit, y?:gunit) draw another image on image
+--- @field pixel fun(self:image, x:integer, y:integer,rgb?:rgb) draw pixel directly on image
+--- @field clr fun(self:image) clear image
+--- @field fill fun(self:image, rgb?:rgb) fill image with color
+--- @field raw fun(self:image):integer[] image return raw pixel data
+--- @field copy fun(self:image):image clones to new image
+
+--- @class connection
+--- @field send fun(self:connection, data:string) send data to connection
+--- @field recv fun(self:connection):string | nil receive data from connection
+--- @field test fun(self:connection):string | nil test if connection is still alive, returns string for error, 'safe close' for no error
+--- @field kill fun(self:connection) close connection
+
+
+--- @type number ~3.1457
+pi = nil
+--- @type number ~6.2914
+tau = nil
+--- @type image image raster for the front screen
+gui = nil
+--- @type image image raster for the back screen or 'sky'
+sky = nil
+
+--- shorthand for gui:text
+function text(...)
+end
+
+--- shorthand for gui:line
+function line(...)
+end
+
+--- shorthand for gui:rect
+function rect(...)
+end
+
+--- shorthand for gui:rrect
+function rrect(...)
+end
+
+--- shorthand for gui:img
+function img(...)
+end
+
+--- shorthand for gui:pixel
+function pixel(...)
+end
+
+--- shorthand for gui:fill
+function fill(...)
+end
+
+--- shorthand for gui:clr
+function clr(...)
+end
+
+-- Absolute value
+---@param f number
+---@return number
+function abs(f)
+end
+
+-- insert model data into an asset <name:string, {v=[float,float,float][],i=int[],u=[float,float][]}>
+---@param asset string
+---@param t model_data
+---@return string stating what mode the model was built in
+function mod(asset, t)
+end
+
+-- Removes an entity
+---@param ent entity | integer
+function kill(ent)
+end
+
+-- Squareroot value
+---@param f number
+---@return number
+function sqrt(f)
+end
+
+-- Load a sub bundle
+---@param str string
+function sub(str)
+end
 
 -- Set the camera position and/or rotation
 ---@param params cam_params
-function cam(params) end
+function cam(params)
+end
 
+-- Set a tile within 3d space. Nil asset deletes.
+---@param asset string
+---@param x integer
+---@param y integer
+---@param z integer
+---@param rot integer?
+function tile(asset, x, y, z, rot)
+end
 
--- Make sound
----@param freq number
----@param length number?
-function sound(freq, length) end
-
-
--- Sine value
+-- Floor value
 ---@param f number
----@return number
-function sin(f) end
+---@return integer
+function flr(f)
+end
 
+-- Check if gamepad button is held down
+---@param button string
+---@return boolean
+function btn(button)
+end
 
--- Make an instrument
----@param freqs number[]
----@param half boolean? subsequent freqs are half the previous  
-function instr(freqs, half) end
-
+-- Crude deletion of a 16x16x16 chunk. Extremely efficient for large area tile changes. Not including arguments delete all tiles.
+---@param x? integer
+---@param y? integer
+---@param z? integer
+function dtile(x, y, z)
+end
 
 -- Check if key is held down
 ---@param key string
 ---@param volatile boolean? only true on first frame of key press
 ---@return boolean
-function key(key, volatile) end
+function key(key, volatile)
+end
 
-
--- Set color of pixel at x,y
----@param x integer 
----@param y integer
----@param rgb number[] | string rgba number array or hex string
-function pixel(x, y, rgb) end
-
-
--- Create new image buffer userdata, does not set as asset
----@param w integer
----@param h integer
----@return image
-function nimg(w, h) end
-
-
--- Clear the draw target
-function clr() end
-
-
--- Check if gamepad button is held down
+-- Check how much a gamepad is pressed, axis gives value between -1 and 1
 ---@param button string
----@return boolean
-function button(button) end
+---@return number
+function abtn(button)
+end
 
+-- Get mouse position, delta, button states, and unprojected vector
+---@return mouse
+function mus()
+end
+
+-- Get a string of all keys pressed
+---@return string
+function cin()
+end
+
+-- Create a new connection to the ip, site, etc. A :port is optional
+--- @param addr string
+--- @return connection
+function conn(addr)
+end
+
+-- Prints string to console
+---@param ... string|number|integer|boolean|image|entity|connection|nil
+function cout(...)
+end
+
+-- Find first occurence of a tile in a given direction
+---@param x integer
+---@param y integer
+---@param z integer
+---@return string
+function gtile(x, y, z)
+end
 
 -- Random float from 0-1, or provide a range
 ---@param a number?
 ---@param b number?
 ---@return number
-function rnd(a, b) end
+function rnd(a, b)
+end
 
+-- Return the asset name of the tile at a given location
+---@param x integer
+---@param y integer
+---@param z integer
+---@return string
+function gtile(x, y, z)
+end
 
--- Removes an entity
----@param ent entity | integer
-function kill(ent) end
+-- Groups an entity onto another entity
+---@param parent entity
+---@param child entity
+function lot(parent, child)
+end
 
+-- Cosine value
+---@param f number
+---@return number
+function cos(f)
+end
 
--- load an overlaying bundle
----@param str string
-function overload(str) end
+-- Set an animation by passing in series of textures
+---@param name string
+---@param items string[]
+---@param speed number?
+function anim(name, items, speed)
+end
 
+-- Set various app state parameters
+---@param attributes attributes?
+function attr(attributes)
+end
+
+-- Sets image data as a texture
+---@param asset string
+---@param im image
+function tex(asset, im)
+end
+
+-- Check if a tile is present at a given location
+---@param x integer
+---@param y integer
+---@param z integer
+---@return boolean
+function istile(x, y, z)
+end
+
+-- Squareroot value
+---@param f number target
+---@param e number raise by
+---@return number
+function pow(f, e)
+end
 
 -- Ceil value
 ---@param f number
 ---@return integer
-function ceil(f) end
+function ceil(f)
+end
 
+-- An imperfect random number generator for integers. May suffer from modulo bias, only i32
+---@param a integer?
+---@param b integer?
+---@return integer
+function irnd(a, b)
+end
+
+-- Make an instrument
+---@param freqs number[]
+---@param half boolean? subsequent freqs are half the previous
+function instr(freqs, half)
+end
+
+-- Resets lua context and reloads all assets and scripts fresh
+function reload()
+end
+
+-- load an overlaying bundle
+---@param str string
+function over(str)
+end
+
+-- Sine value
+---@param f number
+---@return number
+function sin(f)
+end
+
+-- Stop sounds on channel
+---@param channel number
+function mute(channel)
+end
+
+-- List models by search
+---@param model string
+---@param bundle integer?
+---@return string[]
+function lmod(model, bundle)
+end
+
+-- Create new image buffer userdata, does not set as asset
+---@param w integer
+---@param h integer
+---@return image
+function nimg(w, h)
+end
+
+-- Make a sound or note
+---@param freq number
+---@param length number?
+function sound(freq, length)
+end
+
+-- Base 10 logarithm of the value
+---@param f number target
+---@return number
+function log(f)
+end
 
 -- Spawn an entity from an asset
 ---@param asset string
@@ -158,219 +362,21 @@ function ceil(f) end
 ---@param z number
 ---@param scale number?
 ---@return entity
-function spawn(asset, x, y, z, scale) end
-
-
--- Get image buffer userdata for editing or drawing
----@param asset string  
----@return image
-function gimg(asset) end
-
-
--- Hard quit or exit to console
----@param u integer? >0 soft quits
-function quit(u) end
-
-
--- Check if a tile is present at a given location
----@param x integer 
----@param y integer
----@param z integer
----@return boolean
-function istile(x, y, z) end
-
-
--- Set skybox as draw target
-function sky() end
-
-
--- An imperfect random number generator for integers. May suffer from modulo bias, only i32
----@param a integer?
----@param b integer?
----@return integer
-function irnd(a, b) end
-
-
--- Remove all tiles from the world
-function dtiles() end
-
-
--- Draw text on the gui at position
----@param txt string
----@param x number
----@param y number
----@param rgb number[] | string? rgba number array or hex string
----@param typeset string? font name or size 
-function text(txt, x, y, rgb, typeset) end
-
-
--- Sets image data as a texture
----@param asset string
----@param im image
-function tex(asset, im) end
-
-
--- Get mouse position, delta, button states, and unprojected vector
----@return mouse
-function mouse() end
-
-
--- Draw a line on the draw target
----@param x number
----@param y number  
----@param x2 number
----@param y2 number
----@param rgb number[] | string? rgba number array or hex string
-function line(x, y, x2, y2, rgb) end
-
-
--- Check how much a gamepad is pressed, axis gives value between -1 and 1
----@param button string
----@return number
-function analog(button) end
-
-
--- Crude deletion of a 16x16x16 chunk. Extremely efficient for large area tile changes
----@param x integer
----@param y integer
----@param z integer
-function dchunk( x, y, z) end
-
+function make(asset, x, y, z, scale)
+end
 
 -- Make a song
 ---@param notes number[][] | number[] nested array first is frequency, second is length
-function song(notes) end
+function song(notes)
+end
 
-
--- Floor value
----@param f number
----@return integer
-function flr(f) end
-
-
--- Set background color of raster
----@param rgb number[] | string rgba number array or hex string
-function fill(rgb) end
-
-
--- List models by search
----@param model string  
----@param bundle integer?
----@return string[]
-function lmodel(model, bundle) end
-
-
--- Set various app state parameters
----@param attributes Attributes
-function attr(attributes) end
-
-
--- Set an animation by passing in series of textures
----@param name string
----@param items string[]
----@param speed number?
-function anim(name, items, speed) end
-
-
--- Draw image on the gui at position
----@param im image  
----@param x number?  
----@param y number?
-function img(im, x, y) end
-
-
--- Reset lua context
-function reload() end
-
-
--- Groups an entity onto another entity
----@param parent entity
----@param child entity
-function group(parent, child) end
-
-
--- Absolute value
----@param f number
----@return number
-function abs(f) end
-
-
--- Send UDP
--- Coming soon
-
-
--- Draw a rectangle on the draw target
----@param x number
----@param y number
----@param w number
----@param h number
----@param rgb number[] | string? rgba number array or hex string
-function rect(x, y, w, h, rgb) end
-
-
--- Prints string to console
----@param message string
-function log(message) end
-
-
--- Stop sounds on channel
----@param channel number
-function silence(channel) end
-
-
--- Load a sub bundle
----@param str string
-function subload(str) end
-
-
--- insert model data into an asset <name:string, {v=[float,float,float][],i=int[],u=[float,float][]}>
+-- Get image buffer userdata for editing or drawing
 ---@param asset string
----@param t model_data
----@return string stating what mode the model was built in
-function model(asset, t) end
+---@return image
+function gimg(asset)
+end
 
-
--- Draw a rounded rectangle on the draw target
----@param x number
----@param y number
----@param w number
----@param h number
----@param ro number radius of corners
----@param rgb number[] | string? rgba number array or hex string
-function rrect(x, y, w, h, ro, rgb) end
-
-
--- Cosine value
----@param f number  
----@return number
-function cos(f) end
-
-
--- Squareroot value
----@param f number
----@return number
-function sqrt(f) end
-
-
--- Get a string of all keys pressed
----@return string
-function input() end
-
-
--- Recieve UDP
--- Coming soon
-
-
--- Set front screen (gui) as draw target
-function gui() end
-
-
--- Set a tile within 3d space. Nil asset deletes.
----@param asset string
----@param x integer
----@param y integer
----@param z integer
----@param rot integer?
-function tile(asset, x, y, z, rot) end
-
-
+-- Hard quit or exit to console
+---@param u integer? >0 soft quits
+function quit(u)
+end
---
+++ maze/scripts/main.lua
@@ -0,0 +1,136 @@
+sky:fill("FF0")
+
+attr { modernize = false, glitch = { 0.1, 0.5, 0 }, curvature = 0.97, resolution = 480 }
+
+board_size = 2
+function tern(a, b, c)
+    if a then
+        return b
+    else
+        return c
+    end
+end
+
+function board()
+    -- for i = -board_size, board_size do
+    --     for j = -board_size, board_size do
+    --         local v = irnd(3)
+    --         tile(tern(v == 2, 0, "blocks" .. v), i, j, 0)
+    --     end
+    -- end
+    -- tile("blocks2", -board_size - 1, 0, 0)
+    -- tile("blocks3", board_size + 1, 0, 0)
+    local b = { { 8, 6, 4, 2, "blue" },
+        { 10,    12, 14, 16, 18 },
+        { 19,    21, 23, 22, 20 },
+        { 17,    15, 13, 11, 9 },
+        { "red", 1,  3,  5,  7 } }
+
+    local walls = { { { nil, nil }, { nil, -1 }, { nil, -1 }, { nil, -1 }, { nil, -1 } },
+        { { nil, -1 },  { nil, -1 },  { nil, -1 },  { nil, -1 },  { nil, nil } },
+        { { nil, nil }, { nil, -1 },  { nil, -1 },  { nil, -1 },  { nil, -1 } },
+        { { nil, -1 },  { nil, -1 },  { nil, -1 },  { nil, -1 },  { nil, nil } },
+        { { nil, nil }, { nil, nil }, { nil, nil }, { nil, nil }, { nil, nil } } }
+
+    for i = -2, 2 do
+        for j = -2, 2 do
+            local ni = i + 3
+            local nj = 6 - (j + 3)
+            local n = "" .. b[nj][ni]
+            -- local n = ((i + 3) + (j + 2) * 5)
+            block(i * 3, j * 3, 0, n)
+            local w = walls[nj][ni]
+            wall(i * 3, j * 3, 0, w[1], w[2])
+        end
+    end
+end
+
+function pieces()
+    die = make("die", 0, 0, 1)
+    die.offset = { -0.5, -0.5, -0.5 }
+    add_item(die)
+    add_item(guy1)
+    add_item(guy2)
+end
+
+function controls()
+    if key("w") then
+        pos.y = pos.y + sin(rot) * speed
+        pos.x = pos.x + cos(rot) * speed
+    elseif key("s") then
+        pos.y = pos.y - sin(rot) * speed
+        pos.x = pos.x - cos(rot) * speed
+    end
+    if key("a") then
+        pos.y = pos.y + cos(rot) * speed
+        pos.x = pos.x - sin(rot) * speed
+    elseif key("d") then
+        pos.y = pos.y - cos(rot) * speed
+        pos.x = pos.x + sin(rot) * speed
+    end
+    local f = -8
+    if key("q", true) then
+        local x = pos.x - cos(rot) * f
+        local y = pos.y - sin(rot) * f
+        rot = rot + tau / 8
+        print(rot)
+        pos.x = x + cos(rot) * f
+        pos.y = y + sin(rot) * f
+    elseif key("e", true) then
+        local x = pos.x - cos(rot) * f
+        local y = pos.y - sin(rot) * f
+        rot = rot - tau / 8
+        pos.x = x + cos(rot) * f
+        pos.y = y + sin(rot) * f
+    end
+
+    local m = mus()
+    local v = 10
+    local f = -(pos.z) / (m.vz)
+    local p = { x = pos.x + f * m.vx, y = pos.y + f * m.vy, z = 0 + pos.z + f * m.vz }
+    cursor.x = p.x
+    cursor.y = p.y
+    cursor.z = p.z
+    shadow.x = p.x
+    shadow.y = p.y
+
+    if m.m1 then
+        if mouse_reset then
+            mouse_once = true
+            mouse_reset = false
+        else
+            mouse_once = false
+        end
+    else
+        mouse_once = false
+        mouse_reset = true
+        item_release_check()
+    end
+end
+
+speed = 0.1
+pos = { x = 0, y = -8, z = 8 }
+rot = pi / 2
+mouse_reset = true
+mouse_once = false
+
+function main()
+    make_blocks()
+    mod("cursor", { t = { "blocks9" }, q = { { 0, 0, 1 }, { 1, 0, 1 }, { 1, 1, 1 }, { 0, 1, 1 } } })
+    mod("shadow", { t = { "shadow" }, q = { { 0, 1, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 0, 1 } } })
+    cursor = make("cursor", 0, 0, 0)
+    cursor.offset = { -0.5, -.5, -0.4 }
+    shadow = make("shadow", 0, 0, 0)
+    shadow.offset = { -0.5, -.5, -0.45 }
+    cam { pos = { pos.x, pos.y, pos.z }, rot = { rot, -tau / 8 } }
+    mod("die", { t = { "dice0", "dice1", "dice2", "dice3", "dice4", "dice5" } })
+    pieces()
+    board()
+end
+
+function loop()
+    controls()
+    check_items()
+    -- die.rz = die.rz + 0.06
+    cam { pos = { pos.x, pos.y, pos.z }, rot = { rot, -tau / 8 } }
+end
